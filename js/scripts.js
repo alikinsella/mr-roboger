76043bb928ebcc2eeaa9e6dfab141c9a56de654d
Ali Kinsella <ali.kinsella@gmail.com>
2020-05-20 14:57:27 -0500

    add functionality for the first spec, i.e. return a string of numbers when a single number is inputted

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
-// back end logic
+// back-end logic
 var beep = "beep!"
 var boop = "boop!"
 var neighbor = "Won't you be my neighbor?"
-var roboger = function(input) {
-  let str = '';
-  for (let i = 0; i < (input+1); i++) {
-    str = str + i;
-  }
-  var numbers = [str.split("")];
-  console.log(numbers);
-  debugger;
 
+var roboger = function(input) {
+  let numStr = '';
+  for (let i = 0; i <= input; i++) {
+    numStr = numStr + i;
+  };
+  var numbers = [numStr.split("")];
+  return numbers;
 };
 
 
 
-// front end logic
+// front-end logic
 
 $(document).ready(function() {
   $("form#number").submit(function(event) {
@@ -34,33 +33,4 @@ $(document).ready(function() {
 // replace 2 with var
 // search array using index method for 3
 // replace 3 with var
-
-
-
-
-// function pow(x, n) {
-//   let result = 1;
-
-//   // multiply result by x n times in the loop
-//   for (let i = 0; i < n; i++) {
-//     result *= x;
-//   }
-
-//   return result;
-// }
-
-// alert( pow(2, 3) ); // 8
-
-
-
-// function pow(x, n) {
-//   if (n == 1) {
-//     return x;
-//   } else {
-//     return x * pow(x, n - 1);
-//   }
-// }
-
-// alert( pow(2, 3) ); // 8
-
 
