e5a827fb6f6804481e9af5406030be5338b60466
Ali Kinsella <ali.kinsella@gmail.com>
2020-05-20 15:37:00 -0500

    add functionality for specs 2–4

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,13 @@ var roboger = function(input) {
   for (let i = 0; i <= input; i++) {
     numStr = numStr + i;
   };
-  var numbers = [numStr.split("")];
-  return numbers;
+  var numbers = numStr.split("");
+  numbers[numbers.indexOf("1")] = beep;
+  numbers[numbers.indexOf("2")] = boop;
+  numbers[numbers.indexOf("3")] = neighbor;
+
+  return numbers
+
 };
 
 
