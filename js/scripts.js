5e4a09610ffb2a3aba24eb4ef2c6d83a623d268d
Ali Kinsella <ali.kinsella@gmail.com>
2020-05-20 23:18:22 -0500

    add fully functional javascript that meets all specs

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var beep = "beep!"
 var boop = "boop!"
 var neighbor = "Won't you be my neighbor?"
-var poopy = "poopy!"
+
 
 var roboger = function(input) {
   let numbers = [];
@@ -14,16 +14,13 @@ var roboger = function(input) {
     return number.toString();
   });
 
-  // numberStrings[numberStrings.indexOf("3")] = neighbor;
-  // numberStrings[numberStrings.indexOf("2")] = boop;
-  // numberStrings[numberStrings.indexOf("1")] = beep;
   
   var robogers = numberStrings.map(function(numberString) {
-    if (numberString === "3") {
+    if (numberString.includes("3")) {
       return numberString = neighbor;
-    } else if (numberString === "2") {
+    } else if (numberString.includes("2")) {
       return numberString = boop;
-    } else if (numberString === "1") {
+    } else if (numberString.includes("1")) {
       return numberString = beep;
     } else {
       return numberString;
@@ -47,6 +44,4 @@ $(document).ready(function() {
   });
 });
 
-
-// replace a number with any digit of 1, 2, or 3 with corresponding phrase
 
